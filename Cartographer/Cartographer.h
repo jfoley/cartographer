37bb236260d2f296e6a550ee8764b270169747ae
John Foley <john@hisfoleyness.com>
2016-01-21 17:21:54 -0800

    Initial Commit

---
+++ Cartographer/Cartographer.h
@@ -0,0 +1,19 @@
+//
+//  Cartographer.h
+//  Cartographer
+//
+//  Created by john foley on 1/21/16.
+//  Copyright © 2016 john foley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cartographer.
+FOUNDATION_EXPORT double CartographerVersionNumber;
+
+//! Project version string for Cartographer.
+FOUNDATION_EXPORT const unsigned char CartographerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cartographer/PublicHeader.h>
+
+
